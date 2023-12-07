576be0e1176dda54777528365ba469fe65395d16
vicksin2 <vicksin900@gmail.com>
2023-12-07 12:53:35 +0100

    first commit

---
+++ src/main/java/ng/edu/binghamuni/inventory/repository/DrinkRepository.java
@@ -0,0 +1,14 @@
+package ng.edu.binghamuni.inventory.repository;
+
+import ng.edu.binghamuni.inventory.Domain.Drink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DrinkRepository extends JpaRepository<Drink,Long> {
+//    Drink(type); Long(id type)
+//    you can also write your own custom query using: @Query("")
+//    List<Drink> findDrinkByPriceGreaterThan(int price);
+
+//    this repository helps us interact with our database
+}
---
+++ src/main/java/ng/edu/binghamuni/inventory/services/DrinkServiceImpl.java
@@ -0,0 +1,71 @@
+package ng.edu.binghamuni.inventory.services;
+
+import ng.edu.binghamuni.inventory.Domain.Drink;
+import ng.edu.binghamuni.inventory.repository.DrinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DrinkServiceImpl implements DrinkService{
+    @Autowired
+    DrinkRepository drinkRepository;
+    @Override
+    public Drink saveDrink(Drink drink) {
+        return drinkRepository.save(drink);
+    }
+
+
+    @Override
+    public Drink getDrinkById(long id) {
+        Optional<Drink> drink = drinkRepository.findById(id);
+
+        Drink emptyDrink = null;
+        if (drink.isPresent()){
+            emptyDrink = drink.get();
+            return emptyDrink;
+        } else {
+            throw new RuntimeException("Drink Not Found");
+        }
+    }
+
+    @Override
+    public List<Drink> getAllDrink() {
+        return drinkRepository.findAll();
+    }
+
+    @Override
+    public Drink updateDrink(Drink drink) {
+        return null;
+    }
+
+
+    @Override
+    public Drink updateDrinkById(Drink drink) {
+        Optional<Drink> optionalDrink = drinkRepository.findById(drink.getId());
+
+        if (optionalDrink.isPresent()){
+            Drink updateDrink = new Drink();
+            updateDrink.setCapacity(drink.getCapacity());
+            updateDrink.setColor(drink.getColor());
+            updateDrink.setName(drink.getName());
+            updateDrink.setType(drink.getType());
+            updateDrink.setCompany(drink.getCompany());
+            updateDrink.setId(drink.getId());
+            updateDrink.setIngredients(drink.getIngredients());
+
+            drinkRepository.save(updateDrink);
+            return updateDrink;
+        } else{
+            throw new RuntimeException("Drink does not exist");
+        }
+
+    }
+
+    @Override
+    public void deleteDrinkById(long id) {
+        drinkRepository.deleteById(id);
+    }
+}
